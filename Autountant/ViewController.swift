a11cb360f19866ce7a998577dc17c5db725d716d
boomag77 <96821826+boomag77@users.noreply.github.com>
2023-11-19 17:11:19 -0500

    Initial Commit

---
+++ Autountant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autountant
+//
+//  Created by Sergey on 11/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
