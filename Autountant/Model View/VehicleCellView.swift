d98c625a9b7833cf85c8a6c209d3ed3cffd37033
boomag77 <96821826+boomag77@users.noreply.github.com>
2023-11-28 22:52:53 -0500

    temp commit: added check-mark for active vehicle

--- Autountant/Model View/VehicleCellView.swift
+++ Autountant/Model View/VehicleCellView.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class VehicleCellView: UIView {
     
+    var activeVehicle: Bool = false
+    
     private lazy var nameLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .left
-        label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        //label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        //label.adjustsFontForContentSizeCategory = true
         label.textColor = .black
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -21,10 +24,19 @@ class VehicleCellView: UIView {
     private lazy var mileageLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .left
-        label.font = UIFont.systemFont(ofSize: 10, weight: .light)
+        //label.font = UIFont.systemFont(ofSize: 10, weight: .light)
+        //label.adjustsFontForContentSizeCategory = true
         label.textColor = .lightGray
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    private lazy var selectedView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "checkmark")
+        //imageView.contentMode = .scaleAspectFit
+        return imageView
     }()
     
     override init(frame: CGRect) {
@@ -34,6 +46,7 @@ class VehicleCellView: UIView {
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        configure()
     }
     
     func fillLabels(name: String, mileage: String) {
@@ -46,19 +59,31 @@ class VehicleCellView: UIView {
         addSubview(nameLabel)
         addSubview(mileageLabel)
         
-        mileageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        if activeVehicle {
+            addSubview(selectedView)
+            // selected ImageView constraints
+            selectedView.topAnchor.constraint(equalTo: topAnchor, constant: 10.0).isActive = true
+            selectedView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20.0).isActive = true
+            selectedView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10.0).isActive = true
+            selectedView.widthAnchor.constraint(equalTo: selectedView.heightAnchor).isActive = true
+            
+            nameLabel.trailingAnchor.constraint(greaterThanOrEqualTo: selectedView.leadingAnchor, constant: -10.0).isActive = true
+            mileageLabel.trailingAnchor.constraint(greaterThanOrEqualTo: selectedView.leadingAnchor, constant: -10.0).isActive = true
+        }
         
         // nameLabel constraints
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10.0).isActive = true
         nameLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10.0).isActive = true
         
         // mileageLabel constraints
         mileageLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
         mileageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20.0).isActive = true
-        mileageLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        mileageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10.0).isActive = true
         mileageLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        
     }
 
 }
